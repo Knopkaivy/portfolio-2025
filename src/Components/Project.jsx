9900dfafdbe254d9330b9820d7f99d2977855949
TIVY-PK\tivy <tetiana.ivy@concentrix.com>
2025-09-13 08:19:41 -0500

    update ScrollTrigger for Projects

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -1,65 +1,62 @@
-import { useRef, useLayoutEffect } from "react"
+import { useRef, forwardRef,useImperativeHandle,  useLayoutEffect } from "react"
 import gsap from "gsap"
 import '../Styles/Project.scss'
 
-function Project({imgSrc, name}) {
+const Project = forwardRef(({imgSrc, name, url, ind}, ref) => {
     const projectRef = useRef(null);
     const projectImageOverlayRef = useRef(null);
-    const projectImageRef = useRef(null);
     const projectLinkRef = useRef(null);
 
+    useImperativeHandle(ref, () =>{
+        return {
+        root: projectRef.current,
+        imageOverlay: projectImageOverlayRef.current,
+        link: projectLinkRef.current,
+        }
+    },[]);
+
     useLayoutEffect(()=>{
-        let ctx = gsap.context(()=>{
-            gsap.from(projectLinkRef.current,{
-                y: 50,
-                scrollTrigger: {
-                    trigger: projectRef.current,
-                    start: 'top 30%',
-                    end: 'top 0%',
-                    scrub: 1
-                }
-            });
-
-            gsap.from(projectImageOverlayRef.current,{
-                opacity: .1,
-                scrollTrigger: {
-                    trigger: projectRef.current,
-                    start: 'top 50%',
-                    end: 'top 0%',
-                    scrub: 1
-                }
-            });
-
-            gsap.to(projectImageRef.current,{
-                scale: 1.025,
-                scrollTrigger: {
-                    trigger: projectRef.current,
-                    start: 'top 90%',
-                    end: 'top 0%',
-                    scrub: true
-                }
-            });
-        }, projectRef);
-
-        return () => ctx.revert();
+        if(ind === 0){
+            let ctx = gsap.context(()=>{
+                gsap.from(projectLinkRef.current,{
+                    y: 200,
+                    scrollTrigger: {
+                        trigger: projectRef.current,
+                        start: 'top 50%',
+                        end: 'top 0%',
+                        scrub: 1
+                    }
+                });
+                gsap.to(projectImageOverlayRef.current,{
+                    opacity: .5,
+                    scrollTrigger: {
+                        trigger: projectRef.current,
+                        start: 'top 100%',
+                        end: 'top 0%',
+                        scrub: 1
+                    }
+                });
+            }, projectRef);
+            return () => ctx.revert();
+        }
     }, [])
 
     return (  
-        <div ref={projectRef} className="project">
+        <div ref={projectRef} className="project" style={{zIndex: ind}} >
             <div className="project__image-container">
                 <div ref={projectImageOverlayRef} className="project__image-overlay"></div>
-                <img ref={projectImageRef} src={imgSrc} aria-hidden='true' className="project__image" />
+                <img src={imgSrc} aria-hidden='true' className="project__image" />
             </div>
             <div className="project__content-container">
                 <div ref={projectLinkRef} className="project__link-container">
                     <svg height="60" width="800" xmlns="http://www.w3.org/2000/svg">
                         <rect className="project__svg" height="60" width="800" />
                     </svg>
-                    <a href="/" className="project__link">{name}</a>
+                    <a href={url} target="_blank" rel="noopener noreferrer" className="project__link">{name}</a>
                 </div>
             </div>
         </div>
     );
-}
+})
 
 export default Project;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,21 +1,60 @@
-import Project from './Project';
-import ProjectImagesArray from './ProjectImages';
+import { useRef, useLayoutEffect } from "react"
+import gsap from "gsap"
+import Project from './Project'
+import ProjectImagesArray from './ProjectImages'
 import '../Styles/Projects.scss'
+
 function Projects() {
     const projectListArray = [
-        {imgSrc: ProjectImagesArray[0].image, name: 'Beauty Salon Landing Page',},
-        {imgSrc: ProjectImagesArray[1].image, name: 'Hotel Landing Page',},
-        {imgSrc: ProjectImagesArray[2].image, name: 'Photographer Portfolio',},
-        {imgSrc: ProjectImagesArray[3].image, name: 'Real Estate Listing',},
+        {imgSrc: ProjectImagesArray[0].image, name: 'Beauty Salon Landing Page', url: 'https://beauty-salon-landing-demo.web.app/'},
+        {imgSrc: ProjectImagesArray[1].image, name: 'Hotel Landing Page', url: 'https://borgo-santandrea-home-demo.web.app/'},
+        {imgSrc: ProjectImagesArray[2].image, name: 'Photographer Portfolio', url: 'https://photographer-portfolio-demo.web.app/'},
+        {imgSrc: ProjectImagesArray[3].image, name: 'Real Estate Listing', url: 'https://real-estate-landing-demo.web.app/'},
     ]
+
+    const projectsContainerRef = useRef(null);
+    const projectsRef = useRef([]);
+    projectsRef.current = [];
+
+    useLayoutEffect(()=>{
+        if(!projectsRef.current.length) return;
+
+        const ctx = gsap.context(()=>{
+            const tl = gsap.timeline({
+                scrollTrigger:{
+                    trigger: projectsContainerRef.current,
+                    start: 'top 0%',
+                    end: () => '+=' + window.innerHeight * projectsRef.current.length,
+                    scrub: 1,
+                    pin: true,
+                }
+            });
+
+            projectsRef.current.forEach((project, i) => {
+                if(i !== 0 ){
+                    tl.from(project.root, {yPercent: 100 }, '+=0')
+                    .from(project.link,{
+                        y: 200,
+                        duration: .5
+                    }, '<')
+                    .to(project.imageOverlay,{
+                        opacity: .5,
+                        duration: .5
+                    }, '<');
+                }
+            })
+        }, projectsContainerRef);
+        return () => ctx.revert();
+    }, [projectListArray]);
+
     const projectList = projectListArray.map((project, i) =>{
-        return <Project key={i} imgSrc={project.imgSrc} name={project.name} />
+        return <Project key={i} imgSrc={project.imgSrc} name={project.name} url={project.url} ind={i} ref={(el) => (projectsRef.current[i] = el)} />
     })
 
     return (
 
 
-        <div className="projects" >
+        <div ref={projectsContainerRef} className="projects" >
             {projectList}
         </div>
       );
